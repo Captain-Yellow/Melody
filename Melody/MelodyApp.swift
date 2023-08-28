48b587173bbf10e863ea163886e6dba90bbc5ddf
Mohammad Afshar <neo.mohammad.afshar@gmail.com>
2023-08-28 09:51:04 +0330

    Initial Commit

---
+++ Melody/MelodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodyApp.swift
+//  Melody
+//
+//  Created by Mohammad Afshar on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MelodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
