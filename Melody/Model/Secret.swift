45208f9a1bbb5e407d152b7dc211028fc7ca5b52
Mohammad Afshar <50849702+Captain-Yellow@users.noreply.github.com>
2023-08-31 18:27:26 +0330

    Update Secret.swift

--- Melody/Model/Secret.swift
+++ Melody/Model/Secret.swift
@@ -15,13 +15,4 @@ struct Secret {
     static let musicShowComments = ""
     static let musicAddComments = ""
 }
-/*
-struct Secret {
-    static let musicApi = "http://demo.melodio.tech:9880/"
-    static let getMusicInfo = "content/display_content_detail_by_user_type/"
-    static let musicLikeApi = "/content_details/like_content/"
-    static let musicUnlikeApi = "/content_details/unlike_content/"
-    static let musicShowComments = "/comment/music/show_comments/"
-    static let musicAddComments = "/comment/music/add_comment/"
-}
-*/
+
